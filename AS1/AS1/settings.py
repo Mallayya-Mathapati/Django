682865cb6762a49ca249964b88b5be85a7c58a87
Mallayya-Mathapati <72851592+Mallayya-Mathapati@users.noreply.github.com>
2020-12-10 12:59:17 +0530

    Update settings.py

--- AS1/AS1/settings.py
+++ AS1/AS1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'g_m6p&4@a#+#)59u9lbo-*^8e_&2xe(ye62xla950esku+&pv_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000','mallayya.herokuapp.com']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
